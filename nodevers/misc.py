0f82250fcdb7805c7a0f7e394aa57656e5dd2746
Kerem Çakırer <kcakirer@hotmail.com>
2013-08-29 13:27:44 +0300

    Added many get_*_dir() functions and a way to validate versions to nodevers.misc module.

--- nodevers/misc.py
+++ nodevers/misc.py
@@ -4,6 +4,7 @@ This modules includes all the functions that are used very frequently.
 
 import os
 import shutil
+import re
 
 def get_nodevers_prefix():
     """
@@ -45,3 +46,41 @@ def mknodist_prefix(path):
     for i in dirs:
         full_path = os.path.join(path, i)
         os.makedirs(full_path)
+
+def valid_version_string(version):
+    """
+    Use regex to see if version
+    looks valid.
+    """
+    regex = "^(\d+)\.(\d+)\.(\d+)$"
+    match = re.match(regex, version)
+    if match is None:
+        return False
+    else:
+        return True
+
+def get_versions_dir():
+    """
+    Return the path where Nodes will be installed.
+    """
+    return os.path.join(get_nodevers_prefix(), "versions")
+
+def get_bin_dir():
+    """
+    Return the path where the symlink to current Node's bin
+    will be created.
+    """
+    return os.path.join(get_nodevers_prefix(), "bin")
+
+def get_patches_dir():
+    """
+    Return the path where the user should put his patches in.
+    """
+    return os.path.join(get_nodevers_prefix(), "patches")
+
+def get_tmp_dir():
+    """
+    Return the path where nodevers will download source packages
+    and build Node.
+    """
+    return os.path.join(get_nodevers_prefix(), "tmp")
