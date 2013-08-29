5ef738f8f4b96b96b90b0e2aca7bf6fddf5c4988
Kerem Çakırer <kcakirer@hotmail.com>
2013-08-29 20:18:26 +0300

    nodevers.__main__ now tries to create a nodevers directory if it doesn't exist already.

--- nodevers/__main__.py
+++ nodevers/__main__.py
@@ -7,6 +7,13 @@ import sys
 from . import misc
 from . import cli
 
+if not misc.valid_nodevers_prefix(misc.get_nodevers_prefix()):
+    try:
+        misc.mknodevers_prefix(misc.get_nodevers_prefix())
+    except IOError:
+        sys.stderr.write("Error: failed to create the nodevers directory")
+        sys.exit(3)
+
 def main(argv):
     cli.parse(argv[1:])
 
