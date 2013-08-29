d73e56c1a5419572e225c950b14b47f0ef0eb403
Kerem Çakırer <kcakirer@hotmail.com>
2013-08-29 14:53:31 +0300

    The nodevers.versions module is function complete for now.

--- nodevers/versions.py
+++ nodevers/versions.py
@@ -13,7 +13,9 @@ __helpstr__ = """Usage: nodevers versions [-h]
 """
 
 import os
+import sys
 from . import misc
+from . import cli
 
 def get_versions_list():
     """
@@ -24,3 +26,18 @@ def get_versions_list():
     os.chdir(versions_dir)
     versions = [ver for ver in os.listdir(versions_dir) if os.path.isdir(ver)]
     return versions
+
+def parse(args):
+    """
+    Parse the arguments and call the correct functions
+    based on them.
+    """
+    if len(args) == 0:
+        for ver in get_versions_list():
+            sys.stdout.write("%s\n" % ver)
+    elif "-h" in args or "--help" in args:
+        cli.help_func(__helpstr__)
+    else:
+        for i in args:
+            sys.stdout.write("Unknown option: %s\n" % i)
+            cli.help_func(__helpstr__)
