c336cd1a102d39947e0c9af0059ae14c38dc9277
Kerem Çakırer <kcakirer@hotmail.com>
2013-09-11 02:16:00 +0300

    Minor change

--- nodevers/cli.py
+++ nodevers/cli.py
@@ -8,9 +8,11 @@ from nodevers.command import setup_subparsers
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument("-v", "--version", action="version",
-                        version="nodevers %s" % __version__,
-                        help="display the nodevers version")
+    parser.add_argument(
+        "-v", "--version",
+        action="version",
+        version="nodevers %s" % __version__,
+        help="display the nodevers version")
     subparser = parser.add_subparsers(title="Commands", metavar="<command>")
     setup_subparsers(subparser)
     if len(sys.argv) == 1:
